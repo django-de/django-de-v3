404fef07b761fb4315e51214ebd9376554883691
Martin Mahner <martin@mahner.org>
2013-01-17 20:11:56 +0100

    Better static folder names

--- django_de/static/config.rb
+++ django_de/static/config.rb
@@ -1,11 +1,12 @@
 # Require any additional compass plugins here.
+require "susy"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "img"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -14,7 +15,7 @@ javascripts_dir = "javascripts"
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this

d9c95a98815ca31f0165ebf35e5e6c261c7c2a9d
Stephan Jaekel <steph@rdev.info>
2013-01-17 10:21:08 +0100

    Updated settings.

--- django_de/global_settings.py
+++ django_de/global_settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'django.contrib.markup',
     'django.contrib.staticfiles',
 
     'django.contrib.admin',
@@ -68,27 +69,3 @@ INSTALLED_APPS = (
 
     'django_de.news',
 )
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}

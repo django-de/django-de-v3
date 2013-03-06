d899b3dae63a13167cb800815ac3f26db7b951e0
Stephan Jaekel <steph@rdev.info>
2013-03-06 15:53:19 +0100

    Added heroku settings.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn django_de.wsgi
---
+++ django_de/settings_heroku.py
@@ -0,0 +1,34 @@
+from django_de.global_settings import *
+
+import dj_database_url
+
+DATABASES = {'default': dj_database_url.config()}
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+ADMINS = (
+    ('Stephan Jaekel', os.getenv('DJANGO_ADMIN_EMAIL')),
+)
+
+MANAGERS = ADMINS
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+
+TWITTER_USERNAME = 'djangode'
+TWITTER_CONSUMER_KEY = os.getenv('TWITTER_CONSUMER_KEY')
+TWITTER_CONSUMER_SECRET = os.getenv('TWITTER_CONSUMER_SECRET')
+TWITTER_ACCESS_TOKEN_KEY = os.getenv('TWITTER_ACCESS_TOKEN_KEY')
+TWITTER_ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
